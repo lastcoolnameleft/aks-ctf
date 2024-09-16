0aeb3572a191ae9a9e6b13c7ebf515e288d5a3fb
Tommy Falgout <tommy@lastcoolnameleft.com>
2024-09-16 11:21:07 -0500

    capital env vars

--- workshop/attack-1-helper.sh
+++ workshop/attack-1-helper.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 
-### ISSUE:  What is the equivalent of this?  LB with a Public IP?  It appears to be a way to get the Public IP of the first node in the cluster
-the_ip=`kubectl get svc -n dev app -o json | jq -r '.status.loadBalancer.ingress[0].ip'`
+THE_IP=`kubectl get svc -n dev app -o json | jq -r '.status.loadBalancer.ingress[0].ip'`
 
-echo "${the_ip}" | grep '^[0-9][0-9.]*[0-9]$' >> /dev/null
+echo "${THE_IP}" | grep '^[0-9][0-9.]*[0-9]$' >> /dev/null
 if [ $? -ne 0 ]; then
 	echo "Unable to determine cluster NodeIP. Please ask for help."
 	exit 1
@@ -16,7 +15,7 @@ You've got shellz!
 
 0ur syst3m p0pped anoth3r box 4 joo. t3h fundz hav b33n deduct3d fr0m ur accoun7.
 
-Ur n3w comput3r kan B @ccess3d @ http://${the_ip}:8080/webshell us1ng Ur r3gurlar cr3ds. h@ve fUn!
+Ur n3w comput3r kan B @ccess3d @ http://${THE_IP}:8080/webshell us1ng Ur r3gurlar cr3ds. h@ve fUn!
 
 4eva ur pal,
 Natoshi Sakamoto
