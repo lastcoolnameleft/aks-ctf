8bea3c971aa44aba31a28b90bf9fccbf8cccf87e
Tommy Falgout <tommy@lastcoolnameleft.com>
2024-09-18 15:42:33 -0500

    Add steps to attack the app and deploy miner

--- workshop/scenario_1/attack-1-helper.sh
+++ workshop/scenario_1/attack-1-helper.sh
@@ -1,22 +1,23 @@
 #! /bin/sh
 
-THE_IP=`kubectl get svc -n dev dashboard -o json | jq -r '.status.loadBalancer.ingress[0].ip'`
+THE_IP=`kubectl get svc -n dev insecure-app -o json | jq -r '.status.loadBalancer.ingress[0].ip'`
 
 echo "${THE_IP}" | grep '^[0-9][0-9.]*[0-9]$' >> /dev/null
 if [ $? -ne 0 ]; then
-	echo "Unable to determine cluster NodeIP. Please ask for help."
+	echo "Unable to determine LB IP. Please ask for help."
 	exit 1
 fi
 
 cat <<EOF
 Gr8 n3ws, Ha><0r,
 
-You've got shellz!
+We c@n 0wn3 th3 d3v 3nvir0nm3nt. 0ur 1337 h@x0r sk1llz w1ll 3n@bl3 us 2 1nfiltr@t3 th3 3nvir0nm3nt @nd 3xpl0it 1t.
 
-0ur syst3m p0pped anoth3r box 4 joo. t3h fundz hav b33n deduct3d fr0m ur accoun7.
-
-Ur n3w comput3r kan B @ccess3d @ http://${THE_IP}:8080/webshell us1ng Ur r3gurlar cr3ds. h@ve fUn!
+Ur n3w comput3r kan B @ccess3d @ http://${THE_IP}:8080/ . h@ve fUn!
 
 4eva ur pal,
 Natoshi Sakamoto
+
+p.s. 1f u n33d h3lp, ch3ck out http://${THE_IP}:8080/admin and http://${THE_IP}:8080/crash . 1f u n33d m0r3 h3lp, 1t's @ll 0n u. 1'm 0ut.
+
 EOF
